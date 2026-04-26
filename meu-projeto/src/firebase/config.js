d72a99837374eb2eb2098330f61ce5433d6821fe
matheus morais <matheusmorais.dev@gmail.com>
2026-04-26 17:03:40 -0300

    firebase config

---
+++ meu-projeto/src/firebase/config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC_ny2NhAimSPVWj5Euoj_r1u8_cQIn3sM",
+  authDomain: "meu-projeto-a6d28.firebaseapp.com",
+  projectId: "meu-projeto-a6d28",
+  storageBucket: "meu-projeto-a6d28.firebasestorage.app",
+  messagingSenderId: "605291092567",
+  appId: "1:605291092567:web:84ea313c43793f8b6be112",
+  measurementId: "G-1JT3R8SH6Y"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
