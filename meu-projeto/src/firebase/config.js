d6c5b84f4b07fc600ed030328c2aeec8afb079ad
matheus morais <matheusmorais.dev@gmail.com>
2026-04-26 17:23:27 -0300

    alterando a cloud da firebase novamente...

--- meu-projeto/src/firebase/config.js
+++ meu-projeto/src/firebase/config.js
@@ -4,9 +4,9 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyC_ny2NhAimSPVWj5Euoj_r1u8_cQIn3sM",
-  authDomain : "my-project-a6d28.firebaseapp.com" , 
-  projectId : "my-project-a6d28" , 
-  storageBucket : "my-project-a6d28.firebasestorage.app" , 
+  authDomain: "meu-projeto-a6d28.firebaseapp.com",
+  projectId: "meu-projeto-a6d28",
+  storageBucket: "meu-projeto-a6d28.firebasestorage.app",
   messagingSenderId: "605291092567",
   appId: "1:605291092567:web:84ea313c43793f8b6be112",
   measurementId: "G-1JT3R8SH6Y"
